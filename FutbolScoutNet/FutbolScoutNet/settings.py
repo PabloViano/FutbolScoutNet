bb8da91de61019cf577d94f2eef86e739d676180
ignazbrun5@gmail.com <Chalengercampo2001>
2023-08-18 01:32:20 -0300

    Principio

--- FutbolScoutNet/FutbolScoutNet/settings.py
+++ FutbolScoutNet/FutbolScoutNet/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dmd(cig$(ehq9)f*5#kh8qgne)9s0d9e8=+$e8ne9lq^%zlwi)'
+SECRET_KEY = 'django-insecure-!+9hj@^4s!n8k6jts8huwxq^(5hy&)^4#0+)%_bew1^36v$war'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sitio',
 ]
 
 MIDDLEWARE = [
--- FutbolScoutNet/FutbolScoutNet/urls.py
+++ FutbolScoutNet/FutbolScoutNet/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sitio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitio/', views.inicio),
 ]
