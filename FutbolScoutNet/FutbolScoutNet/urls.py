a47a516e56b73a62c6c9bbaf99ef35a09cef37c7
Msotelo1 <msotelo111201@gmail.com>
2023-08-18 12:01:20 -0300

    Log in creado

--- FutbolScoutNet/FutbolScoutNet/urls.py
+++ FutbolScoutNet/FutbolScoutNet/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from sitio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', views.inicio),
-    path('publicacion/', views.form_publicacion)
+    path('publicacion/', views.form_publicacion),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
