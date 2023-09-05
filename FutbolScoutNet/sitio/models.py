6c40840eef7de75e15f80cffd739b0f891d61efa
Pablo <pabloviano13@gmail.com>
2023-09-05 11:41:31 -0300

    Tabs

--- FutbolScoutNet/sitio/models.py
+++ FutbolScoutNet/sitio/models.py
@@ -29,10 +29,10 @@ class Post(models.Model):
     verificado = models.BooleanField(default=False)
 
     def str(self):
-    if self.user:
-        return f'Post: {self.titulo[:20]} de {self.user.username}'
-    else:
-        return f'Post: {self.titulo[:20]} (Usuario no especificado)'
+        if self.user:
+            return f'Post: {self.titulo[:20]} de {self.user.username}'
+        else:
+            return f'Post: {self.titulo[:20]} (Usuario no especificado)'
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
